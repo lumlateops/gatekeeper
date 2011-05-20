f51ff942f084bac648ab1fb94b2abd6d678e1024
Prachi Gupta <praachee@gmail.com>
2011-05-19 22:35:35 -0700

    experimenting with packages etc

--- src/com/deallr/gatekeeper/emailHandler/gmailHandler.py
+++ src/com/deallr/gatekeeper/emailHandler/gmailHandler.py
@@ -1,2 +1,12 @@
 __author__ = 'prachi'
-  
+
+
+# Gets the user email and initiates the auth flow.
+# This method is provides the response for the request to the frontend
+#class addGmailHandler:
+#    def GET (self, email):
+#        return email
+
+# Handles the complete flow for Gmail
+class GmailHandler:
+    # Stores the auth token in the database
--- src/com/deallr/gatekeeper/requestBroker/defaultBroker.py
+++ src/com/deallr/gatekeeper/requestBroker/defaultBroker.py
@@ -1,2 +1,29 @@
 __author__ = 'prachi'
-  
+
+
+import web
+
+# All the endpoints exposed by the app
+urls = (
+'/email/add/gmail/(.*)', 'addGmailHandler',
+'/(.*)', 'defaultHandler'
+)
+
+app = web.application(urls, globals())
+
+# This is the default handler that handles all un-registered end points
+class defaultHandler:
+    def GET (self, name):
+        return web.websafe('Sorry, I dont understand your request')
+
+
+# Gets the user email and initiates the auth flow.
+# This method is provides the response for the request to the frontend
+class addGmailHandler:
+    def GET (self, email):
+        return email
+    
+
+# The main method
+if __name__ == "__main__":
+    app.run()
