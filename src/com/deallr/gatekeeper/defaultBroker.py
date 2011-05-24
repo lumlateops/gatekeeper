1090d3fc954724207928c2e7147ebb7613076aca
Prachi Gupta <praachee@gmail.com>
2011-05-23 19:17:26 -0700

    more gmail oauth stuff

--- src/com/deallr/gatekeeper/defaultBroker.py
+++ src/com/deallr/gatekeeper/defaultBroker.py
@@ -9,6 +9,7 @@ import yahooHandler
 # All the endpoints exposed by the app
 urls = (
 '/email/add/gmail/(.*)', 'addGmailHandler',
+'/get_access_token/(.*)', 'gmailCallbackHandler',
 '/email/add/msn/(.*)', 'addMSNHandler',
 '/email/add/aol/(.*)', 'addAOLHandler',
 '/email/add/yahoo/(.*)', 'addYahooHandler',
@@ -16,6 +17,8 @@ urls = (
 )
 
 app = web.application(urls, globals())
+
+gmailHandler1 = None
 
 # This is the default handler that handles all un-registered end points
 class defaultHandler:
@@ -27,8 +30,14 @@ class defaultHandler:
 # This method is provides the response for the request to the frontend
 class addGmailHandler:
     def GET (self, email):
-        handler = gmailHandler.GmailHandler()
-        return gmailHandler.GmailHandler.getAuthToken(handler, email)
+        gmailHandler1 = gmailHandler.GmailHandler()
+        return gmailHandler.GmailHandler.getAuthToken(gmailHandler1, email)
+
+class gmailCallbackHandler:
+    def GET (self, oauth_token):
+        print 'outh_token:'
+        print oauth_token
+        return gmailHandler.GmailHandler.callback(gmailHandler1, oauth_token)
 
 # Gets the user email and initiates the auth flow.
 # This method is provides the response for the request to the frontend
