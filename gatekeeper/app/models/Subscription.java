b1e07cb2bee26c2ab4e702e968abec9fa70ee138
Prachi Gupta <praachee@gmail.com>
2011-09-11 09:35:56 -0700

    Changes to subscription table

--- gatekeeper/app/models/Subscription.java
+++ gatekeeper/app/models/Subscription.java
@@ -17,37 +17,18 @@ import play.db.jpa.Model;
 public class Subscription extends Model
 {
 	@OneToOne
-	public UserInfo		userInfo;
-	@OneToOne
-	public Retailers		retailer;
-	@OneToOne
 	public Department	department;
+	public Long				accountId;
 	public Boolean		active;
 	public Date				createdAt;
 	public Date				updatedAt;
 	
-	public Subscription(UserInfo userInfo, Retailers retailer,
-			Department department, Boolean active, Date createdAt, Date updatedAt)
+	public Subscription(Department department, Long accountId, Boolean active, Date createdAt, Date updatedAt)
 	{
-		this.userInfo = userInfo;
-		this.retailer = retailer;
 		this.department = department;
+		this.accountId = accountId;
 		this.active = active;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
-
-	/**
-	 * CREATE TABLE `Subscriptions` (
-		`id` bigint(20) NOT NULL AUTO_INCREMENT,
-		`userAccountId` int(11) NOT NULL, 
-		`retailerId` int(11) NOT NULL,
-		`departmentId` int(11) NOT NULL,
-		`active` bit(1) NOT NULL,
-		`createdAT` datetime DEFAULT NULL,
-		`updatedAT` datetime DEFAULT NULL,
-		PRIMARY KEY (`id`),
-		UNIQUE KEY `index_subscription` (`userAccountId`,`retailerId`,`departmentId`)
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-	 */
 }
