61c68dac91ae9596eb5d683c61faa791add88a85
Prachi Gupta <praachee@gmail.com>
2011-11-13 18:39:30 -0800

    Adding more email category to deal fetch

--- gatekeeper/app/controllers/DealController.java
+++ gatekeeper/app/controllers/DealController.java
@@ -39,8 +39,8 @@ public class DealController extends BaseContoller
 {
 	private static final int		PAGE_SIZE									= Integer.parseInt((String)Play.configuration.get("deal.page.size"));
 	private static final String	USER_DEAL_LOOKUP_HQL			= "SELECT d AS d FROM Deal d WHERE d.userInfo.id IS ? AND d.dealEmail.emailCategory.id IS 1 ORDER BY ";
-	private static final String	USER_DEAL_COUNT_HQL				= "SELECT count(d) FROM Deal d WHERE d.userInfo.id IS ? AND d.dealEmail.emailCategory.id IS 1";
-	private static final String	UNREAD_DEAL_COUNT_HQL			= "SELECT count(d) FROM Deal d WHERE d.userInfo.id IS ? AND d.dealEmail.emailCategory.id IS 1 AND d.createdAt > ?";
+	private static final String	USER_DEAL_COUNT_HQL				= "SELECT count(d) FROM Deal d WHERE d.userInfo.id IS ? AND d.dealEmail.emailCategory.id IN (1, 5)";
+	private static final String	UNREAD_DEAL_COUNT_HQL			= "SELECT count(d) FROM Deal d WHERE d.userInfo.id IS ? AND d.dealEmail.emailCategory.id IN (1, 5) AND d.createdAt > ?";
 	private static final String BULK_MARK_DEAL_READ				= "UPDATE Deal d SET d.dealRead = true WHERE d IN (:deals)"; 
 	private static final String	DEAL_LOOKUP_HQL						= "SELECT d AS d FROM Deal d WHERE d.userInfo.id IS ? AND d.id IN ";
 	private static final String	UNREAD_DEAL_LOOKUP_HQL		= "SELECT d AS d FROM Deal d WHERE d.userInfo.id IS ? AND d.dealRead='false' ";
